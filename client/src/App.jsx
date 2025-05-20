277069e204e07b07d29e34519cf6b6ef22f6a5ac
Deep-Sorathiya-007 <deepsorathiya803@gmail.com>
2025-05-20 22:22:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Navbar from './components/Navbar' 
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
+import MyBooking from './pages/MyBooking';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
+
+
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
+      
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/rooms' element={<AllRooms/>} />
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
+          <Route path='/my-bookings' element={<MyBooking/>} />
+          <Route path='/owner' element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="add-room" element={<AddRoom />} />
+              <Route path="list-room" element={<ListRoom />} />              
+          </Route>
+          
+
+        </Routes>
+      </div>
+      <Footer></Footer> {/* all pages per Footer.jsx aavshe pan tena mate tene <Routes> ni bhar lakhavanu*/}
+    </div>
+  )
+}
+
+{/* Home.jsx ma jetla mount karavasho to te .jsx vali te files only honme page per j visible hashe pan Navbar.jsx and Footer.jsx mate hu App.jsx ma mount karavish jethi badha pages per show case karshe */}
+export default App
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+connectDB()
+
+const app = express()
+app.use(cors()) // enables us to vross-origin resorce sharing
+
+
+//middelware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+//API for clerk webhook
+app.use("/api/clerk", clerkWebhooks)
+
+app.get('/', (req, res) => res.send("API is working GOOD!!"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
